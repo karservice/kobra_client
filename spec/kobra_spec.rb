fbbe5b26adfb84c55e83565efd397ffb2b2c5071
Johan Eckerström <johan@duh.se>
2011-06-15 22:34:57 +0200

    Typo

--- spec/kobra_spec.rb
+++ spec/kobra_spec.rb
@@ -7,10 +7,10 @@ describe Kobra::Client do
       stub_request(:post, "http://john:a9b8c7@kobra.ks.liu.se/students/api.json").
         with(:body => "email=johdoXXX%40student.liu.se").
         to_return(fixture('student_found.txt'))
-    
+
       kobra = Kobra::Client.new(:username => 'john', :api_key => 'a9b8c7')
       student = kobra.get_student(:email => 'johdoXXX@student.liu.se')
-      
+
       student[:email].should           == 'johdoXXX@student.liu.se'
       student[:liu_id].should          == 'johdoXXX'
       student[:rfid_number].should     == '9999999999'
@@ -21,39 +21,39 @@ describe Kobra::Client do
       student[:union].should           == 'LinTek'
       student[:personal_number].should == '887711-0011'
     end
-    
-    it 'failes to obtain student information by email' do
+
+    it 'fails to obtain student information by email' do
       stub_request(:post, "http://john:a9b8c7@kobra.ks.liu.se/students/api.json").
         with(:body => "email=johdoXXX%40student.liu.se").
         to_return(fixture('student_not_found.txt'))
-    
+
       kobra = Kobra::Client.new(:username => 'john', :api_key => 'a9b8c7')
       expect { kobra.get_student(:email => 'johdoXXX@student.liu.se') }.to raise_error Kobra::Client::NotFound
     end
-    
-    it 'failes to authorize' do
+
+    it 'fails to authorize' do
       stub_request(:post, "http://john:wrong@kobra.ks.liu.se/students/api.json").
         with(:body => "email=johdoXXX%40student.liu.se").
         to_return(fixture('auth_failure.txt'))
-    
+
       kobra = Kobra::Client.new(:username => 'john', :api_key => 'wrong')
       expect { kobra.get_student(:email => 'johdoXXX@student.liu.se') }.to raise_error Kobra::Client::AuthError
     end
-    
+
     it 'gets garbage' do
       stub_request(:post, "http://john:a9b8c7@kobra.ks.liu.se/students/api.json").
         with(:body => "email=johdoXXX%40student.liu.se").
         to_return(fixture('garbage.txt'))
-    
+
       kobra = Kobra::Client.new(:username => 'john', :api_key => 'a9b8c7')
       expect { kobra.get_student(:email => 'johdoXXX@student.liu.se') }.to raise_error Kobra::Client::BadResponse
     end
-    
+
     it 'gets server error' do
       stub_request(:post, "http://john:a9b8c7@kobra.ks.liu.se/students/api.json").
         with(:body => "email=johdoXXX%40student.liu.se").
         to_return(fixture('server_error.txt'))
-    
+
       kobra = Kobra::Client.new(:username => 'john', :api_key => 'a9b8c7')
       expect { kobra.get_student(:email => 'johdoXXX@student.liu.se') }.to raise_error Kobra::Client::ServerError
     end
